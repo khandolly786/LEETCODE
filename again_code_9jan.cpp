9f72a760128a3df03cc3c4ac989691ca4144cef0
Darakhshan Naheed <136336115+khandolly786@users.noreply.github.com>
2025-01-09 12:32:00 +0530

    Create again_code_9jan.cpp

---
+++ again_code_9jan.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+        int n=words.size();
+        vector<string> result;
+        for(int i=0; i<n; i++){
+            for(int j=0;j<n;j++){
+                if(i!=j and words[j].find(words[i])!=string::npos){
+                    result.push_back(words[i]);
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+};
