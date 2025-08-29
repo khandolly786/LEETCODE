7d6f2e8743c55681b4121e4bfade95aece8d3816
Darakhshan Naheed <136336115+khandolly786@users.noreply.github.com>
2025-08-29 22:45:07 +0530

    Create devops.js

---
+++ devops.js
@@ -0,0 +1,8 @@
+Day 7/100 – #NationSkillUp Challenge 🚀
+Today I explored Disk Partitioning in Linux. 💽🐧
+Learned how partitions help in organizing data, improving system performance, and managing resources efficiently — an essential skill for system admins and DevOps engineers. ⚡
+
+👉 Course Link: https://www.geeksforgeeks.org/batch/skill-up-devops/track/su-devops-day5/article/NTQyNjk4
+
+#skillupwithgfg #nationskillup
+Tag: @GeeksforGeeks
