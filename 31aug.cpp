8d79efea17bd6dbbbeb7ae46abeb0437427c0490
Darakhshan Naheed <136336115+khandolly786@users.noreply.github.com>
2025-08-31 22:11:23 +0530

    Create 31aug.cpp

---
+++ 31aug.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int maxWater(vector<int>& arr) {
+        int left = 0, right = arr.size() - 1;
+        int res = 0;
+        while (left < right) {
+
+            // find the water stored in the container between
+            // arr[left] and arr[right]
+            int water = min(arr[left], arr[right]) * (right - left);
+            res = max(res, water);
+
+            if (arr[left] < arr[right])
+                left += 1;
+            else
+                right -= 1;
+        }
+
+        return res;
+    }
+};
